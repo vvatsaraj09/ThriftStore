d737cf9dbbaa33459c80f84778a27d0847a12289
Jahnvi Shah <jahnvishah15@gmail.com>
2020-10-25 23:09:31 +0530

    deleted the mess

--- ThriftStore-master-2/followers/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Followers
-# Register your models here.
-admin.site.register(Followers)
--- ThriftStore-master-2/followers/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Followers(models.Model):
-    follower  =  models.CharField(max_length=120)
-    following =  models.CharField(max_length=120)
